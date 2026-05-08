1d7c15c4e9030fc96bf2824b1cf9bdc1c487f457
AbrilCC <abrilelisiricc@gmail.com>
2026-05-08 18:54:16 -0300

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import "./bot/telegramBot.js";
+//import "./bot/telegramBot.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, "https://alertavencimientos.vercel.app"]
+  origin: "https://alertavencimientos.vercel.app"
 }));
 app.use(express.json());
 
