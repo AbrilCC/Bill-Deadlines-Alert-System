7c3699e0bbb9eb3ce1554dab21970ab9ac399694
AbrilCC <abrilelisiricc@gmail.com>
2026-05-08 19:10:04 -0300

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,4 @@
-import express from "express";
+/*import express from "express";
 import cors from "cors";
 import "./bot/telegramBot.js";
 import dotenv from "dotenv";
@@ -41,3 +41,30 @@ app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
+*/
+
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("API funcionando");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("API funcionando");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
