440143d46a58ae0a1670a28c0ee827950db2282e
AbrilCC <abrilelisiricc@gmail.com>
2026-05-10 16:08:21 -0300

    minor changes

--- backend/src/routes/upload.routes.js
+++ backend/src/routes/upload.routes.js
@@ -1,8 +1,10 @@
     import express from "express";
+    import multer from "multer";
     import { uploadManualInvoiceController } from "../controllers/upload.controller.js";
     import authMiddleware from "../middleware/auth.middleware.js";
 
     const router = express.Router();
+    const upload = multer();
 
     router.post("/manual-invoice", authMiddleware, upload.single("invoice"), uploadManualInvoiceController);
 
